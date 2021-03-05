7e7f697ead3b924caee16c2b617158bdf748438e
Levi Pires <leviapires16@gmail.com>
2021-03-05 16:48:11 -0300

    chore: purge playground

--- tunnel.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const tunnel = require('localtunnel');
-
-tunnel({ port: 8080, subdomain: 'playground-motion-jota' }).then(tn => {
-  // eslint-disable-next-line no-console
-  console.log(tn.url);
-
-  process.on('exit', tn.close);
-});
